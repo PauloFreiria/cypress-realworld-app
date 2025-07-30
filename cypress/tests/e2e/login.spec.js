0e3776a643d5273af01d9affb33701033bc267e3
Paulo Freiria <paulofreiria01@gmail.com>
2025-07-30 10:46:15 -0300

    Added SignUp success and failure tests

--- cypress/tests/e2e/login.spec.js
+++ cypress/tests/e2e/login.spec.js
@@ -19,4 +19,27 @@ describe('Real World App Test', () => {
 
   })
 
+  it('Sign Up - Success', () => {
+    loginPage.accessLoginPage();
+    loginPage.signInNewUser(
+      userData.signUpUser.firstName,
+      userData.signUpUser.lastName,
+      userData.signUpUser.username,
+      userData.signUpUser.password,
+      userData.signUpUser.confirmPassword
+    );
+  })
+
+  it('Sign Up - Failure', () => {
+    loginPage.accessLoginPage();
+    loginPage.signInWrongPassword(
+      userData.signUpWrongPassword.firstName,
+      userData.signUpWrongPassword.lastName,
+      userData.signUpWrongPassword.username,
+      userData.signUpWrongPassword.password,
+      userData.signUpWrongPassword.confirmPassword
+    );
+    loginPage.checkSignUpError();
+  })
+ 
 })
--- cypress/tests/pages/loginPage.js
+++ cypress/tests/pages/loginPage.js
@@ -1,11 +1,19 @@
+import { last } from "lodash"
+
 class LoginPage {
     
     selectorsList() {
         const selectors = {
             usernameField: "[name='username']",
             passwordField: "[name='password']",
-            loginButton: "[type='submit']",
+            signInButton: "[type='submit']",
             wrongCredentialAlert: "[role='alert']",
+            singUpField: "[href='/signup']",
+            firstNameField: "[name='firstName']",
+            lastNameField: "[name='lastName']",
+            confirmPasswordField: "[name='confirmPassword']",
+            wrongPasswordFormAlert: "[data-test='signup-password']",
+
         }
         return selectors
     }
@@ -17,13 +25,39 @@ class LoginPage {
     loginWithUser(username, password) {
         cy.get(this.selectorsList().usernameField).type(username)
         cy.get(this.selectorsList().passwordField).type(password)
-        cy.get(this.selectorsList().loginButton).click()
+        cy.get(this.selectorsList().signInButton).click()
     }
 
     checkLoginError() {
 
         cy.get(this.selectorsList().wrongCredentialAlert)
     }
+
+    signInNewUser(firstName, lastName, username, password, confirmPassword) {
+        cy.get(this.selectorsList().singUpField).click()
+        cy.get(this.selectorsList().firstNameField).type(firstName)
+        cy.get(this.selectorsList().lastNameField).type(lastName)
+        cy.get(this.selectorsList().usernameField).type(username)
+        cy.get(this.selectorsList().passwordField).type(password)
+        cy.get(this.selectorsList().confirmPasswordField).type(confirmPassword)
+        cy.get(this.selectorsList().signInButton).click()
+    }
+
+    signInWrongPassword(firstName, lastName, username, password, confirmPassword) {
+        cy.get(this.selectorsList().singUpField).click()
+        cy.get(this.selectorsList().firstNameField).type(firstName)
+        cy.get(this.selectorsList().lastNameField).type(lastName)
+        cy.get(this.selectorsList().usernameField).type(username)
+        cy.get(this.selectorsList().passwordField).type(password)
+        cy.get(this.selectorsList().confirmPasswordField).type(confirmPassword)
+
+    }
+
+    checkSignUpError() {
+        cy.get(this.selectorsList().wrongPasswordFormAlert)
+    }
+
+
 }
 
 export default LoginPage
